f95cc8765ad38d5f4175e52161d9b81f844f1ea4
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-09-03 18:38:26 +0530

    Bug Detected : Deployed to undefined.

--- TRACKING/scripts/deploy.js
+++ TRACKING/scripts/deploy.js
@@ -9,11 +9,14 @@ import hre from "hardhat";
 
 const main = async() => {
   const Tracking = await hre.ethers.getContractFactory("Tracking");
+  // console.log("Contract Factory:", Tracking);
+
   const tracking = await Tracking.deploy();
+  // console.log("Contract Deployment:", tracking);  
 
-  await tracking.waitForDeployment();
-
-  console.log(`Tracking deployed to ${tracking.address}`);
+  // await tracking.waitForDeployment();
+  const receipt =  await tracking.waitForDeployment();
+  console.log(`Tracking deployed to ${receipt.address}`);
 };
 
 // We recommend this pattern to be able to use async/await everywhere
