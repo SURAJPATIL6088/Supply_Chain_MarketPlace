c551d701c6dd4a2c5b69ee1b0007144703595eb4
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-09-03 19:16:50 +0530

    Done Components setup

---
+++ TRACKING/src/Components/index.js
@@ -0,0 +1,39 @@
+// IMPORT ALL THE COMPONENTS THAT ARE PRESENT IN THESE FOLDER
+import Footer from "./Footer";
+import Table from "./Table";
+import NavBar from "./NavBar";
+import Form from "./Form";
+import Services from "./Services";
+import Profile from "./Profile";
+import GetShipment from "./GetShipment";
+import CompleteShipment from "./CompleteShipment";
+import StartShipment from "./StartShipment";
+
+//  IMPORT SVG FILES
+import Nav1 from "./SVG/Nav1";
+import Nav2 from "./SVG/Nav2";
+import Nav3 from "./SVG/Nav3";
+import Fot1 from "./SVG/Fot1";
+import Fot2 from "./SVG/Fot2";
+import Str1 from "./SVG/Str1";
+
+// EXPORT ALL THE COMPONENTS
+export {
+    Footer,
+    Table,
+    NavBar,
+    Form,
+    Services,
+    Profile,
+    GetShipment,
+    CompleteShipment,
+    StartShipment,
+
+    // EXPORT SVG FILES
+    Nav1,
+    Nav2,
+    Nav3,
+    Fot1,
+    Fot2,
+    Str1,
+};
