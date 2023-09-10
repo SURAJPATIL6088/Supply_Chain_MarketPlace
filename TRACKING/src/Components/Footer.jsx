069c01a5278bd19daf928dc18d27359213141eb3
SURAJ BALARAM PATIL <78692972+SURAJPATIL6088@users.noreply.github.com>
2023-09-10 22:57:25 +0530

    Footer Component Added

--- TRACKING/src/Components/Footer.jsx
+++ TRACKING/src/Components/Footer.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
+import {Fot1, Fot2} from "../Components/index";
+
 
 const Footer = () => {
+
+  const footerNavs = [
+    {
+      href:"javascript:void()",
+      name:"Terms",
+    },
+    {
+      href:"javascript:void()",
+      name:"License",
+    },
+    {
+      href:"javascript:void()",
+      name:"Privacy",
+    },
+    {
+      href:"javascript:void()",
+      name:"About us",
+    },
+  ];
+
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className='pt-10'>
+      <div className='max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8'>
+        <div className='justify-between sm:flex'>
+          <div className='space-y-6'>
+            <img src='https://www.floatui.com/logo.svg' className='w-32' />
+            <p className='max-w-md'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nesciunt eum, incidunt illum facilis iure.
+            </p>
+            <ul className='flex flex-wrap items-center gap-4 text-sm sm:text-base'>
+              {footerNavs.map((item, idx) => (
+                <li key={idx} className='text-gray-800 hover:text-gray-500 duration-150'>
+                  <a href={item.href}>
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className='mt-6'>
+            <p className='text-gray-700 font-semibold'>Get the app</p>
+            <div className='flex items-center gap-3 mt-3 sm:block'>
+              <a href='javascript:void()'>
+                <Fot1 />
+              </a>
+
+              <a href='javascript:void()' className='mt-0 block sm:mt-3'>
+                <Fot2 />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-10 py-10 border-t md:text-center'>
+          <p>@2023 Suraj Patil. All rights reserved.</p>
+        </div>
+      </div>      
+    </footer>
+
+  );
+};
 
 export default Footer;
